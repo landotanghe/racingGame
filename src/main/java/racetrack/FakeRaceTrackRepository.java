4149c71fb2d798d9ae3e14296bd88ead4e4ab433
Lando Tanghe <lando.tanghe@gmail.com>
2017-12-10 18:22:33 +0100

    FakeTrackRepo returns square

--- src/main/java/racetrack/FakeRaceTrackRepository.java
+++ src/main/java/racetrack/FakeRaceTrackRepository.java
@@ -19,8 +19,17 @@ public class FakeRaceTrackRepository implements IRaceTrackRepository{
 
     public FormattedTile[][] getRaceTrack(int raceId) {
         try {
-            FormattedTile[][] tiles = new FormattedTile[1][1];
-            tiles[0][0] = new FormattedTile("verticaal_start.png", "VS", new StandardUnderground());
+            FormattedTile[][] tiles = new FormattedTile[3][3];
+            tiles[1][0] = new FormattedTile("verticaal_start.png", "VS", new StandardUnderground());
+            tiles[0][0] = new FormattedTile("L1.png", "VS", new StandardUnderground());
+            tiles[0][1] = new FormattedTile("horizontaal.png", "H", new StandardUnderground());
+            tiles[0][2] = new FormattedTile("L2.png", "VS", new StandardUnderground());
+            tiles[1][2] = new FormattedTile("verticaal.png", "VS", new StandardUnderground());
+            tiles[2][2] = new FormattedTile("L3.png", "VS", new StandardUnderground());
+            tiles[2][1] = new FormattedTile("horizontaal.png", "H", new StandardUnderground());
+            tiles[2][0] = new FormattedTile("L0.png", "H", new StandardUnderground());
+            
+            tiles[1][1] = new FormattedTile("empty.png", "E", new StandardUnderground());
             return tiles;
         } catch (FileNotFoundException ex) {
             Logger.getLogger(FakeRaceTrackRepository.class.getName()).log(Level.SEVERE, null, ex);
